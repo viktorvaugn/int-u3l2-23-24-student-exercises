20d2580d99a061bb417891943ebce4553386237e
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-18 12:24:25 -0400

    Add storing elements code exercise

---
+++ script.js
@@ -0,0 +1,8 @@
+// 1. Take a look at these variables - how are they different than what we've seen?
+let paragraph = document.querySelector("p");
+let img = document.querySelector("img");
+
+
+// 2. Remove the comment backslashes from the code below. What happened?
+// paragraph.innerHTML = "Here's a photo of a bear.";
+// img.src = "https://bearwise.org/wp-content/uploads/2022/09/the-over-the-shoulder-pose-scaled-736x414.jpg"; 
---
+++ storingElements.js
@@ -0,0 +1,24 @@
+// CODE ALONG
+// 1. Create a variable called `painting1` that stores the html element with the class `painting-1` using querySelector.
+
+
+
+// CODE SOLO
+// 2. Create a variable called `painting2` that stores the html element with the class `painting-2` using querySelector.
+
+// 3. Create a variable called `painting3` that stores the html element with the class `painting-3` using querySelector.
+
+// 4. Create a variable called `painting4` that stores the html element with the class `painting-4` using querySelector.
+
+// 5. Create a variable called `title` that stores the title tag with using querySelector.
+
+
+
+
+
+// 6. Once tasks 1-5 are complete, uncomment the code below.
+// painting1.style.width = "500px";
+// painting2.style.animation = "mynewmove 10s infinite";
+// painting3.style.animation = "dance 10s infinite";
+// painting4.style.border = " 10px solid #0fd623";
+// title.innerHTML = "MUHAHAHA!";
